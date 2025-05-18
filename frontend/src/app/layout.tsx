8455faa63cc5d3b7de77b9625824497a3fea3c45
Dennis Peter <peterdennis573@gmail.com>
2025-05-18 11:04:42 +0300

    First commit

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Outfit } from "next/font/google";
+
+const outfit = Outfit({
+  variable: "--font-outfit",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Your App Name",
+  description: "Your app description here",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={outfit.variable}>
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
